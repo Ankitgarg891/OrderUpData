9ef2518434c897c7bb4c100322f412928ff918c6
dipanshukr <dipanshu29sep@gmail.com>
2017-10-01 14:46:56 +0530

    Menu

---
+++ app/src/main/java/com/example/food/orderup/MenuActivity.java
@@ -0,0 +1,25 @@
+package com.example.food.orderup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
