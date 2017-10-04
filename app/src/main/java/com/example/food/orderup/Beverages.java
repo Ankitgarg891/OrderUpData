09a42278e6de314bf3ea37e08ec3dbd9c38371d7
Mayankb10 <31565822+Mayankb10@users.noreply.github.com>
2017-10-04 20:11:59 +0530

    hi

---
+++ app/src/main/java/com/example/food/orderup/Beverages.java
@@ -0,0 +1,30 @@
+package com.example.food.orderup;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Beverages extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_beverages,container,false);
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        //what view to create for user
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //downcasting, attach listeners
+
+
+    }
+}
--- app/src/main/java/com/example/food/orderup/MenuActivity.java
+++ app/src/main/java/com/example/food/orderup/MenuActivity.java
@@ -1,20 +1,68 @@
 package com.example.food.orderup;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
 
+    ListView menulist;
+    ViewPager viewPager;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    int icons[]={R.mipmap.apzicon,R.mipmap.dessicon,R.mipmap.mainicon,R.mipmap.bevicon};
+    String name1[]={"Vegetarian Spring Rolls","Vegetable Fried Wonton","Crispy Chilli Potatoes","Chilli Paneer","Crispy Tangy Mushrooms"
+    ,"Corn Pepper Salt"};
+    int images1[]={R.drawable.roll,R.drawable.wonton,R.drawable.chilli,R.drawable.paneer,R.drawable.mushroom,R.drawable.corn};
+    int price1[]={225,225,245,275,245,260};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        menulist=(ListView)findViewById(R.id.menulist);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        viewPager=(ViewPager)findViewById(R.id.viewpager);
+        tabLayout=(TabLayout)findViewById(R.id.tabmenu);
+
+        tabLayout.setupWithViewPager(viewPager);
+        //ATTACHING FRAGMENTS
+        setUpPager(viewPager);
+        setTabIcons();
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        menulist.setAdapter(new CustomAdapter(this,name1,images1,price1));
+        // set for diff tabs
+
+    }
+
+    void setTabIcons(){
+        tabLayout.getTabAt(0).setIcon(icons[0]);
+        tabLayout.getTabAt(1).setIcon(icons[1]);
+        tabLayout.getTabAt(1).setIcon(icons[2]);
+        tabLayout.getTabAt(1).setIcon(icons[3]);
+    }
+    void setUpPager(ViewPager viewPager)
+    {
+
+        PagerFunctionality pagerFunctionality=new PagerFunctionality(getSupportFragmentManager());
+        pagerFunctionality.add(new Appetizer(),"Appetizer");
+        pagerFunctionality.add(new Dessert(),"Dessert");
+        pagerFunctionality.add(new MainMenu(),"MainMenu");
+        pagerFunctionality.add(new Beverages(),"Beverages");
+        viewPager.setAdapter(pagerFunctionality);
     }
 
     @Override
@@ -22,4 +70,36 @@ public class MenuActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+class PagerFunctionality extends FragmentPagerAdapter
+{
+    ArrayList<Fragment> fragmentList=new ArrayList();//TYPECASTING
+    ArrayList<String> titleList= new ArrayList<>();
+    public PagerFunctionality(FragmentManager fm) {
+        super(fm);
+    }
+
+    void add(Fragment fragment,String title){
+
+        fragmentList.add(fragment);
+        titleList.add(title);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+
+    @Override
+    public int getCount() {
+
+        return fragmentList.size();
+    }
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        return titleList.get(position);
+        //return null
+    }
+
+}
 }
