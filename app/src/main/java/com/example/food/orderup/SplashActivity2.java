2a75787eeed48ec3bfc3b537fb1ce17f90be96ee
Mayankb10 <31565822+Mayankb10@users.noreply.github.com>
2017-10-10 21:28:59 +0530

    hi

---
+++ app/src/main/java/com/example/food/orderup/SplashActivity2.java
@@ -0,0 +1,53 @@
+package com.example.food.orderup;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SplashActivity2 extends AppCompatActivity {
+
+    ImageView tray;
+    Animation animation,fade;
+    TextView text;
+    Handler handler1;
+    Boolean check = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+
+        text=(TextView)findViewById(R.id.order);
+        tray=(ImageView)findViewById(R.id.splashtray);
+        animation= AnimationUtils.loadAnimation(this,R.anim.anim);
+
+   tray.startAnimation(animation);
+
+        handler1 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (check == false) {
+                    startActivity(new Intent(SplashActivity2.this, MainActivity.class));
+                    finish();
+                }
+            }
+
+        }, 2000);}
+
+        @Override
+        public void onBackPressed() {
+            check=true;
+            super.onBackPressed();
+        }
+    }
+
+
