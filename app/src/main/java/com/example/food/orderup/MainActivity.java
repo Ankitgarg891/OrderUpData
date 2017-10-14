0758c604cf464f2a0f2498f9b902358b3a2497d0
ankitgarg <ankitgarg891@gmail.com>
2017-10-15 00:47:10 +0530

    new

--- app/src/main/java/com/example/food/orderup/MainActivity.java
+++ app/src/main/java/com/example/food/orderup/MainActivity.java
@@ -33,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent=new Intent(MainActivity.this,chef_login.class);
+
+                startActivity(intent);
+
             }
         });
 
---
+++ app/src/main/java/com/example/food/orderup/order_list_chef.java
@@ -0,0 +1,16 @@
+package com.example.food.orderup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class order_list_chef extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list_chef);
+
+
+
+    }
+}
