150d421f76751c07dfc70d10059572ccfff1993d
ankitgarg <ankitgarg891@gmail.com>
2017-10-16 00:45:16 +0530

    new

--- app/src/main/java/com/example/food/orderup/Chef_LoginActivity.java
+++ app/src/main/java/com/example/food/orderup/Chef_LoginActivity.java
@@ -4,16 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class Chef_LoginActivity extends AppCompatActivity {
 
     Button view_orders;
 
+    Spinner s1;
+    ArrayAdapter a1;
+
+    String res;
+    String res_name[]={"Town Hall Restaurant", "The Big Chill Café", "Yellow Brick Road Restaurant", "Wok in the Clouds", "The Coffee Bean & Tea Leaf", "Café Turtle", "Omazoni"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_login);
+
+        Spinner s1=(Spinner)findViewById(R.id.spinner);
+
+        a1=new ArrayAdapter(this,R.layout.spinner_layout,res_name);
+
+        s1.setAdapter(a1);
+
+        s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                res=res_name[i];
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                res=res_name[0];
+
+            }
+        });
+
+
+
+
+
+
+
 
         view_orders = (Button) findViewById(R.id.view_orders);
 
@@ -21,6 +58,7 @@ public class Chef_LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Chef_LoginActivity.this, OrderListChefActivity.class);
+                intent.putExtra("res",res);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/food/orderup/OrderListChefActivity.java
+++ app/src/main/java/com/example/food/orderup/OrderListChefActivity.java
@@ -1,17 +1,30 @@
 package com.example.food.orderup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class OrderListChefActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    TextView res_name;
+    String res;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list_chef);
+
+        Intent i=getIntent();
+        res_name=(TextView)findViewById(R.id.res_name);
+
+
+        res=i.getStringExtra("res");
+        res_name.setText(res);
+
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
